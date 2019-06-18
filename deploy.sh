22903c53259f131c449294c5789b67ecc71f4c7d
javivalle98 <51970651+javivalle98@users.noreply.github.com>
2019-06-18 22:44:28 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker kill debian redis > /dev/null 2>&1
-docker rm debian redis > /dev/null 2>&1
+#docker kill debian redis > /dev/null 2>&1
+#docker rm debian redis > /dev/null 2>&1
 docker-compose up -d
